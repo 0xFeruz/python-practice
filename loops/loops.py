33e0e7959df508c3e6dbacde0180655d943e152f
Poopoomanlol <151405316+Poopoomanlol@users.noreply.github.com>
2026-09-26 17:53:53 +0100

    while and else to say hi

--- loops/loops.py
+++ loops/loops.py
@@ -11,3 +11,11 @@
 
 
 # print('hi\n' * 3, end='')
+
+hiTimes = int(input('how mant hi times?'))
+while hiTimes < 1:
+    hiTimes = int(input("more hi times"))
+
+else:
+    print('hi\n' * hiTimes, end='')
+    
